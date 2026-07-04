126837c2210fafdc4a8ffb97e26bee38462f3904
JeniaD <jeniadeshchenko@gmail.com>
2026-07-04 16:36:09 +0200

    vga support

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Luna is experimental simple operating system designed for one single task: host and serve AI models.
 
-Naive idea is that by stripping everything except the job itself, we might achieve improved performance. In reality, this idea is not unique and was already implemented by number of other projects. However, this is educational project, with no intention of competing with modern commercial solutions
+Naive idea is that by stripping everything except the job itself, we might achieve improved performance. In reality, this idea is not unique and was already implemented by number of other projects. This, meanwhile, is an educational project, with no intention of competing with modern commercial solutions
 
 ---
 
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static helloworld: &[u8] = b"Hello from kernel";
-
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in helloworld.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello, \n  -- kernel");
+    // use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
     loop {}
 }
